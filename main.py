e65752b74604bf453345bc07781bf01f4a1b4cd4
Arnoldo R. L. Fuentes <anyback@gmail.com>
2013-09-15 09:49:39 -0500

    crear la estructura

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+map = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+
 def si(preg):
     from string import lower
     resp = lower(raw_input(preg))
     return (resp[0] == 's')
 
-def main():
-	while True:
-		if not si(Quieres iniciar la partida?):
-			break
+def tirar(player,pos):
+	ind = pos.split(",")
+	n = int(ind[0]) - 1
+	m = int(ind[1]) - 1
 
-		print "x | x | x "
-		print "__________"
-		print "x | x | x "
-		print "__________"
-		print "x | x | x "
-		break
+	if player == 1:
+		map[m][n] = "x"
+	else:
+		map[m][n] = "o"
+
+def check():
+	print "hola"
+
+def printcrux():
+	print map[0][0]+" | "+map[0][1]+" | "+map[0][2]+" "
+	print "----------"
+	print map[1][0]+" | "+map[1][1]+" | "+map[1][2]+" "
+	print "----------"
+	print map[2][0]+" | "+map[2][1]+" | "+map[2][2]+" "
+
+def main():
+	i=0
+	while True:
+		pass
+		np = i % 2 + 1
+		pos = str(raw_input("jugador "+str(np)+" (ejemplo 2,3): "))
+		tirar(np,pos)
+		check()
+		printcrux()
+
+		i= i + 1
 
 if __name__ == '__main__':
-	main()
+	while True:
+		if si("Quieres iniciar la partida? "):
+			main()
+		else:
+			break
